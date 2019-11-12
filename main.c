470b9ba9be27c67ebda0d842eb920465813b240d
hyunsongyeon <55427981+hyunsongyeon@users.noreply.github.com>
2019-11-12 15:49:11 +0900

    03

--- main.c
+++ main.c
@@ -5,21 +5,19 @@
 
 int main(int argc, char *argv[])
 {
-	char *pc;
-	int *pi;
+	char buffer[8];
 	double *pd;
+	int *pi;
 	
-	pc = (char*)10000;
-	pi = (int*)10000;
-	pd = (double*)10000;
+	pd = (double*)buffer;
+	*pd = 3.14;
+	printf("%f\n", *(double*)buffer);
 	
-	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	pi = (int*)buffer;
+	*pi = 123;
+	*(pi+1) = 456;
+	printf("%d %d\n", *(int*)buffer, *((int*)buffer +1));
 	
-	pc++;
-	pi++;
-	pd++;
-	
-	printf("증가 후 : pc = %d, pc = %i, pd = %d\n", pc, pi, pd);
 	
 	return 0;
 }
